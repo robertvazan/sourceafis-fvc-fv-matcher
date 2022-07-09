4dd7b212400bca7d18b11b1af61bc539f38aec14
Robert Važan <robert.vazan@tutanota.com>
2022-07-09 17:38:38 +0000

    Project configuration

---
+++ scripts/configure.py
@@ -0,0 +1,16 @@
+# This script generates and updates project configuration files.
+
+# We are assuming that project-config is available in sibling directory.
+# Checkout from https://github.com/robertvazan/project-config
+import pathlib
+project_directory = lambda: pathlib.Path(__file__).parent.parent
+config_directory = lambda: project_directory().parent/'project-config'
+exec((config_directory()/'src'/'fvc.py').read_text())
+
+benchmark_name = lambda: 'Fingerprint Verification'
+benchmark_abbreviation = lambda: 'FV'
+benchmark_url = lambda: 'https://biolab.csr.unibo.it/FVCOnGoing/UI/Form/BenchmarkAreas/BenchmarkAreaFV.aspx'
+is_matcher_part = lambda: True
+inception_year = lambda: 2022
+
+generate()
