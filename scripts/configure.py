3f839c6568a2b06fbd5000e5bbc51a0bb707337e
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 17:47:13 +0000

    Updated project configuration: renamed COPYRIGHT file, project properties

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,17 +1,16 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'fvc.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-benchmark_name = lambda: 'Fingerprint Verification'
-benchmark_abbreviation = lambda: 'FV'
-benchmark_url = lambda: 'https://biolab.csr.unibo.it/FVCOnGoing/UI/Form/BenchmarkAreas/BenchmarkAreaFV.aspx'
-is_matcher_part = lambda: True
-bundled_sister_projects = lambda: ['sourceafis-fvc-fv-extractor']
-inception_year = lambda: 2022
+class Project(scaffold.Fvc):
+    def script_path_text(self): return __file__
+    def benchmark_name(self): return 'Fingerprint Verification'
+    def benchmark_abbreviation(self): return 'FV'
+    def benchmark_url(self): return 'https://biolab.csr.unibo.it/FVCOnGoing/UI/Form/BenchmarkAreas/BenchmarkAreaFV.aspx'
+    def is_matcher_part(self): return True
+    def bundled_sister_projects(self): return ['sourceafis-fvc-fv-extractor']
+    def inception_year(self): return 2022
+    def project_status(self): return self.stable_status()
 
-generate()
+Project().generate()
